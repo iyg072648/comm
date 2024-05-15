7331d2a3beb466980692a3a0cbfbbfdb3c94475b
inyeong <inyeong>
2024-05-15 21:24:04 +0900

    board 정리 및 마무리 (아직 첨부파일 해결 안됨) 파일은 올라가나 이미지 url이 해결 안됨.

--- src/main/java/com/talk/comm/controller/BoardController.java
+++ src/main/java/com/talk/comm/controller/BoardController.java
@@ -21,29 +21,6 @@ public class BoardController {
 
     private final BoardService service;
 
-    @GetMapping("/test")
-    public String tset(Model model) {
-
-        model.addAttribute("cnt", service.boardCount());
-        model.addAttribute("test", service.BoardList());
-
-        return "boards/index";
-    }
-
-    //추가
-    @GetMapping("/main")
-    public String main(Model model) {
-        model.addAttribute("test", service.BoardList());
-        return "boards/main";
-    }
-
-    @GetMapping("/view")
-    public String viewBoard(Model model, int boardId){
-        model.addAttribute("halo", service.getBoard(boardId));
-
-        return "boards/view";
-    }
-
     @GetMapping("/save")
     public String save(){
         return "save";
--- src/main/java/com/talk/comm/service/BoardService.java
+++ src/main/java/com/talk/comm/service/BoardService.java
@@ -1,9 +1,7 @@
 package com.talk.comm.service;
 
-import com.talk.comm.domain.Board;
 import com.talk.comm.dto.BoardDTO;
 import com.talk.comm.dto.BoardFileDTO;
-import com.talk.comm.mapper.BoardMapper;
 import com.talk.comm.repository.BoardRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -18,8 +16,8 @@ import java.util.List;
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
 public class BoardService {
+
     private final BoardRepository boardRepository;
-    private final BoardMapper boardMapper;
 
     public void save(BoardDTO boardDTO) throws IOException {
         if (boardDTO.getBoardFile().get(0).isEmpty()){
@@ -57,18 +55,6 @@ public class BoardService {
 
     public List<BoardDTO> findAll(){
         return boardRepository.findAll();
-    }
-
-    public int boardCount() {
-        return boardMapper.boardCount();
-    }
-
-    public List<Board> BoardList() {
-        return boardMapper.getList();
-    }
-
-    public Board getBoard(int boardId){
-        return boardMapper.getBoard(boardId);
     }
 
     public void updateHits(Long id) {
