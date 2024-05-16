bcc3c74e6410145e27f7c2f2b32a95fad8d9c769
USER\D503 <D503>
2024-05-16 10:47:55 +0900

    이미지 오류 해결

--- src/main/java/com/talk/comm/config/WebConfig.java
+++ src/main/java/com/talk/comm/config/WebConfig.java
@@ -1,19 +1,26 @@
 package com.talk.comm.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@EnableWebMvc
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
-//    private String resourcePath = "/upload/**"; // view에서 사용할 경로
-//
-//    private String savePath = "file:///C:/development/intellij_community/spring_upload_files/";
+    // view에서 사용할 경로
+    private String resourcePath = "/upload/**"; 
 
+    //실제 경로
+    private String savePath = "file:///C:/development/";
+
+    //정석 리소스 핸들러를 추가하는 메서드
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry){
-        registry.addResourceHandler("/upload/**")
-                .addResourceLocations("file:///C:/development/");
+        //upload/** 경로로 들어오는 모든 요청을 처리하기 위한 리소스 핸들러 등록
+        registry.addResourceHandler(resourcePath)
+                //실제 이미지를 찾을 경로 설정
+                .addResourceLocations(savePath);
     }
 }
