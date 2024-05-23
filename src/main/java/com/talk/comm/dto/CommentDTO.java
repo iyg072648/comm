45cf72aae6fb8722db6f113439834762d2a99d0e
USER\D503 <D503>
2024-05-23 11:14:15 +0900

    댓글 작업중

---
+++ src/main/java/com/talk/comm/dto/CommentDTO.java
@@ -0,0 +1,21 @@
+package com.talk.comm.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@ToString
+public class CommentDTO {
+
+    private Long id;
+    private Long boardId;
+    private String commentWriter;
+    private String commentContents;
+    private LocalDateTime createdAt;
+    private Boolean deleteYn;
+
+}
---
+++ src/main/java/com/talk/comm/service/CommentService.java
@@ -0,0 +1,8 @@
+package com.talk.comm.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class CommentService {
+    
+}
