47ee73bcd0a377089c42fbd23e9ab6538317bf5f
inyeong <inyeong>
2024-05-26 01:04:07 +0900

    service,repository 정리중

--- src/main/java/com/talk/comm/repository/CommentRepository.java
+++ src/main/java/com/talk/comm/repository/CommentRepository.java
@@ -1,8 +1,29 @@
 package com.talk.comm.repository;
 
+import com.talk.comm.dto.CommentDTO;
+import lombok.RequiredArgsConstructor;
+import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
+@RequiredArgsConstructor
 public class CommentRepository {
-    
+
+    private final SqlSessionTemplate sql;
+
+    public CommentDTO save(CommentDTO commentDTO){
+        sql.insert("Comment.save", commentDTO);
+        return commentDTO;
+    }
+
+    public List<CommentDTO> findAll(){
+        return sql.selectList("Comment.findAll");
+    }
+
+    public CommentDTO findById(Long id){
+        return sql.selectOne("Comment.findById", id);
+    }
+
 }
--- src/main/java/com/talk/comm/service/CommentService.java
+++ src/main/java/com/talk/comm/service/CommentService.java
@@ -1,8 +1,31 @@
 package com.talk.comm.service;
 
+import com.talk.comm.dto.CommentDTO;
+import com.talk.comm.repository.CommentRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.io.IOException;
+import java.util.List;
 
 @Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class CommentService {
-    
+
+    private final CommentRepository commentRepository;
+
+    public void save(CommentDTO commentDTO) throws IOException{
+        commentRepository.save(commentDTO);
+    }
+
+    public List<CommentDTO> findAll(){
+        return commentRepository.findAll();
+    }
+
+    public CommentDTO findById(Long id){
+        return commentRepository.findById(id);
+    }
+
 }
